7e07e6b1d80b68d511955911113ea8f2213f3db5
Jaden White <jaw65980@email.vccs.edu>
2025-03-15 15:15:13 -0400

    fixing issues

--- Running Time of Search Algorithms Jaden White/Running Time of Search Algorithms Jaden White.cpp
+++ Running Time of Search Algorithms Jaden White/Running Time of Search Algorithms Jaden White.cpp
@@ -14,11 +14,14 @@ private:
             int mid = low + ((high - low) / 2);
             if (arr[mid] == target) {
                 return mid;
+                cout << "foundrec" << endl;
             }
             else if (arr[mid] < target) {
+                cout << "donerec1" << endl;
                 return recursiveBinarySearch(arr, target, mid + 1, high);
             }
             else {
+                cout << "donerec2" << endl;
                 return recursiveBinarySearch(arr, target, low, mid - 1);
             }
         }
@@ -35,12 +38,15 @@ private:
             int mid = low + ((high - low) / 2);
             if (arr[mid] == target) {
                 return mid;
+                cout << "foundit" << endl;
             }
             else if (arr[mid] < target) {
                 low = mid + 1;
+                cout << "doneit1" << endl;
             }
             else {
                 high = mid - 1;
+                cout << "doneit2" << endl;
             }
         }
         return -1;
@@ -61,7 +67,8 @@ public:
     vector<int> arr = {};
 
     void recursivesearch() {
-        int recursiveresult = recursiveBinarySearch(arr, target, 0, (arr.size() - 1));
+        int recursiveresult = recursiveBinarySearch(arr, target, 0, arr.size());
+        cout << recursiveresult << endl;
         if (recursiveresult == -1) {
             cout << "Recusive result not found." << endl;
         }
@@ -72,6 +79,7 @@ public:
 
     void iterbinsearch() {
         int iterbinresult = iterativeBinarySearch(arr, target);
+        cout << iterbinresult << endl;
         if (iterbinresult == -1) {
             cout << "Iterative result not found." << endl;
         }
@@ -82,6 +90,7 @@ public:
 
     void seqsearch() {
         int sequentialresult = sequentialSearch(arr, target);
+        cout << sequentialresult <<endl;
         if (sequentialresult == -1) {
             cout << "Sequential result not found." << endl;
         }
@@ -102,7 +111,7 @@ int main()
     int Vectorend = 0;
 
     cout << "Vector: {";
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < 100; i++) {
         Vectorend++;
         searchtype.arr.push_back(random_number);
         cout << searchtype.arr[i] << ", ";
